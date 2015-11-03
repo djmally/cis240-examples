148aee73a263e7e2de3c32abcfb4730d41f9cce6
David Mally <djmally@gmail.com>
2015-11-03 17:56:05 -0500

    Add comment about include guards to linked_file.h

--- sample_c/linked_file.h
+++ sample_c/linked_file.h
@@ -1,3 +1,13 @@
+// The two lines below, and the #endif at the end of the file, are known as
+// "include guards". They prevent problems that arise from including a file more
+// than once. Consider this simple case: file_one.c includes file_one.h and
+// file_two.h, and file_two.h in turn includes file_one.h, because it references
+// a type defined in that header. Without include guards, file_one.h will be
+// included more than once by file_one.c, and the compiler will complain. The two
+// lines below will prevent this by stopping this header from being compiled if
+// the symbol __LINKED_FILE_H__ is already defined, and defining it if it is not.
+// Note that you DO NOT need these for .c files, as you should never be including
+// a .c file from another .c file.
 #ifndef __LINKED_FILE_H__
 #define __LINKED_FILE_H__
 
